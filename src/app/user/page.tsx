52aa2ae28c4554b800a0126357d769fc7a9f695d
panutat.p <pp.panutat@gmail.com>
2024-12-15 21:46:42 +0700

    display staff using MUI table

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,13 @@
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 import db from '@/app/lib/mysql_connection'
 import { asc } from 'drizzle-orm'
 import { staff } from '@/db/schema'
@@ -18,12 +25,28 @@ export default async function UserPage() {
           <Typography variant="h5">Total: {staffCount}</Typography>
         </Box>
         <Box mt={2} ml={3}>
-          {staffCount > 0 &&
-            staffs.map((staff) => (
-              <Typography key={staff.staffId}>
-                {staff.firstName} {staff.lastName}
-              </Typography>
-            ))}
+          {staffCount > 0 && (
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>First Name</TableCell>
+                    <TableCell>Last Name</TableCell>
+                    <TableCell>Email</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {staffs.map((staff) => (
+                    <TableRow key={staff.staffId}>
+                      <TableCell>{staff.firstName}</TableCell>
+                      <TableCell>{staff.lastName}</TableCell>
+                      <TableCell>{staff.email}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
         </Box>
       </Container>
     </>
