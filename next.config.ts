a79b4d055b4ce9d5ed1640c41a8ef1fc00fb59aa
panutat.p <pp.panutat@gmail.com>
2024-12-22 23:11:15 +0700

    Add next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // !! WARN !!
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
